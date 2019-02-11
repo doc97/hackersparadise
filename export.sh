abe81bab61e975007a7e355660d0be4b9412946b
Daniel Riissanen <daniel.riissanen@gmail.com>
2019-02-11 20:28:07 +0200

    Update export script

--- export.sh
+++ export.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+NAME=HackersParadise
 OUTPUT_DIR=build/output
 EXPORT_DIR=build/export
-NAME=HackersParadise
+ZIP_DIR=build/zip
+ZIP_NAME=${NAME}-$(date +%d-%b-%Y).zip
 
 ./build.sh
 
@@ -17,6 +19,16 @@ echo "Creating .exe file..."
 cat love/love.exe ${EXPORT_DIR}/${NAME}.love > ${EXPORT_DIR}/${NAME}.exe
 cp love/* ${EXPORT_DIR}/
 
+echo "Removing .love file..."
+rm ${EXPORT_DIR}/${NAME}.love
+
+echo "Copying README..."
+cp include/README.txt ${EXPORT_DIR}
+
+echo "Zipping folder..."
+zip -9 -r ${ZIP_DIR}/${ZIP_NAME} ${EXPORT_DIR}/ 1>/dev/null
+
 echo "Done."
 echo
 echo "The build can be found at: ${EXPORT_DIR}/${NAME}.exe"
+echo "The zip can be found at: ${ZIP_DIR}/${ZIP_NAME}"
---
+++ include/README.txt
@@ -0,0 +1,11 @@
+Welcome to Hacker's Paradise!
+
+To start the game you will need to enter a username and a password. It helps
+if you are familiar with the Unix command line, but everything is explained
+in the manual. To open the manual, run the command 'man'.
+
+Username: root
+Password: toor
+
+I hope you enjoy the demo!
+- doc97
